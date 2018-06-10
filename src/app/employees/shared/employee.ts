ff278e01d9d9b525a5ce2777f2f2c79d28e4e239
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-10 07:30:52 +0545

    Add files via upload

--- src/app/employees/shared/employee.ts
+++ src/app/employees/shared/employee.ts
@@ -1,3 +1,16 @@
+// export class Employee {
+//     // constructor(
+//     //     public name: string,
+//     //     public position: string,
+//     //     public department: string,
+//     //     public salary: string
+//     // ){}
+//     name: string;
+//     position: string;
+//     department: string;
+//     salary: string;
+// }
+
 export class Employee {
     firstName: string;
     lastName: string;
